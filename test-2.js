90a0ec0768534b4cf2a20917b232ea84e5547e45
Saul van der Walt <saulvdw@gmail.com>
2018-06-21 14:01:51 +0200

    tests "run" so should still be fine, bit old but let's give it new life

--- test-2.js
+++ test-2.js
@@ -1,78 +1,65 @@
-const test = require('ava');
-const {
-    eventsys,
-    observable
-} = require('./craft-observable.js');
+const test = require('ava')
+const {eventsys, observable} = require('./craft-observable.js')
 
 test('eventsys', t => {
-    t.plan(1);
-    let notifier = eventsys();
+  t.plan(1)
+  let notifier = eventsys()
 
-    notifier.on('test', n => {
-        t.is(n, 'it works!');
-    });
+  notifier.on('test', n => {
+    t.is(n, 'it works!')
+  })
 
-    notifier.emit('test', 'it works!');
-
-});
+  notifier.emit('test', 'it works!')
+})
 
 test('observable:isObservable', t => {
-    t.true(observable().isObservable, 'observable.isObservable is not true');
-});
+  t.true(observable().isObservable, 'observable.isObservable is not true')
+})
 
 test('observable:get', t => {
-    t.plan(3);
+  t.plan(3)
 
-    let gettest = observable();
+  let gettest = observable()
 
-    gettest.$get('fruit', () => "there aren't any fruit");
+  gettest.$get('fruit', () => "there aren't any fruit")
 
-    t.truthy(gettest.fruit, 'proxy based getters are broken');
+  t.truthy(gettest.fruit, 'proxy based getters are broken')
 
-    t.truthy(gettest.get('fruit'), 'observable.get(key) not working');
+  t.truthy(gettest.get('fruit'), 'observable.get(key) not working')
 
-    gettest.fruit = 'Pineapple';
+  gettest.fruit = 'Pineapple'
 
-    t.not(gettest.fruit, 'Pineapple', 'getter return values are not sticking!, also pineapples :P');
-
-});
+  t.not(gettest.fruit, 'Pineapple', 'getter return values are not sticking!, also pineapples :P')
+})
 
 test('observable:set', t => {
-    t.plan(4);
+  t.plan(4)
 
-    let settest = observable();
+  let settest = observable()
 
-    settest.$set('name', (key, value, obj) => {
-        t.is(key, 'name');
-        return 'My name is ' + value;
-    });
+  settest.$set('name', (key, value, obj) => {
+    t.is(key, 'name')
+    return 'My name is ' + value
+  })
 
-    settest.name = 'Bob';
+  settest.name = 'Bob'
 
-    t.is(settest.name, 'My name is Bob', 'His name is not Bob, proxy based setters broken');
+  t.is(settest.name, 'My name is Bob', 'His name is not Bob, proxy based setters broken')
 
-    settest.set('name', 'Bernie');
+  settest.set('name', 'Bernie')
 
-    t.is(settest.get('name'), 'My name is Bernie', 'His name is not Bob, .set(key,val) setters broken');
-
-});
-
+  t.is(settest.get('name'), 'My name is Bernie', 'His name is not Bob, .set(key,val) setters broken')
+})
 
 test('observable:nested', t => {
-    t.plan(3);
+  t.plan(3)
 
-    let base = observable({
-        nest1: {
-            nest2: {}
-        }
-    });
+  let base = observable({ nest1: { nest2: {} } })
 
-    t.true(base.nest1.isObservable)
+  t.true(base.nest1.isObservable)
+  t.true(base.nest1.nest2.isObservable)
 
-    t.true(base.nest1.nest2.isObservable)
+  base.nest1.nest2.nest3 = {}
 
-    base.nest1.nest2.nest3 = {};
-
-    t.true(base.nest1.nest2.nest3.isObservable)
-
-});
+  t.true(base.nest1.nest2.nest3.isObservable)
+})
--- test.js
+++ test.js
@@ -1,100 +1,90 @@
-const test = require('ava');
-const {
-    eventsys,
-    observable
-} = require('./craft-observable-es5.js');
+const test = require('ava')
+const {eventsys, observable} = require('./craft-observable-es5.js')
 
 test('eventsys', t => {
-    t.plan(4);
-    let notifier = eventsys();
+  t.plan(4)
+  let notifier = eventsys()
 
-    let i = 0, x = 0;
-    notifier.on('test', n => {
-        if(!i) t.is(n, 'it works!');
-        i += 1;
-    });
+  let i = 0, x = 0
+  notifier.on('test', n => {
+    if (!i) t.is(n, 'it works!')
+    i += 1
+  })
 
-    notifier.once('test', n => {
-        if(!x) t.is(n, 'it works!');
-        x += 1;
-    });
+  notifier.once('test', n => {
+    if (!x) t.is(n, 'it works!')
+    x += 1
+  })
 
-    notifier.emit('test', 'it works!');
-    notifier.emit('test', 'it works!');
-    notifier.emit('test', 'it works!');
-    notifier.emit('test', 'it works!');
+  notifier.emit('test', 'it works!')
+  notifier.emit('test', 'it works!')
+  notifier.emit('test', 'it works!')
+  notifier.emit('test', 'it works!')
 
-    t.is(i,4);
-    t.is(x,1);
-});
+  t.is(i, 4)
+  t.is(x, 1)
+})
 
 test('observable:isObservable', t => {
-    t.true(observable().isObservable, 'observable.isObservable is not true');
-});
+  t.true(observable().isObservable, 'observable.isObservable is not true')
+})
 
 test('observable:get', t => {
-    t.plan(3);
+  t.plan(3)
 
-    let gettest = observable();
+  let gettest = observable()
 
-    gettest.$get('fruit', () => "there aren't any fruit");
+  gettest.$get('fruit', () => "there aren't any fruit")
 
-    t.truthy(gettest.fruit, 'proxy based getters are broken');
+  t.truthy(gettest.fruit, 'proxy based getters are broken')
 
-    t.truthy(gettest.get('fruit'), 'observable.get(key) not working');
+  t.truthy(gettest.get('fruit'), 'observable.get(key) not working')
 
-    gettest.fruit = 'Pineapple';
+  gettest.fruit = 'Pineapple'
 
-    t.not(gettest.fruit, 'Pineapple', 'getter return values are not sticking!, also pineapples :P');
-
-});
+  t.not(gettest.fruit, 'Pineapple', 'getter return values are not sticking!, also pineapples :P')
+})
 
 test('observable:set', t => {
-    t.plan(4);
+  t.plan(4)
 
-    let settest = observable();
+  let settest = observable()
 
-    settest.$set('name', (key, value, obj) => {
-        t.is(key, 'name');
-        return 'My name is ' + value;
-    });
+  settest.$set('name', (key, value, obj) => {
+    t.is(key, 'name')
+    return 'My name is ' + value
+  })
 
-    settest.name = 'Bob';
+  settest.name = 'Bob'
 
-    t.is(settest.name, 'My name is Bob', 'His name is not Bob, proxy based setters broken');
+  t.is(settest.name, 'My name is Bob', 'His name is not Bob, proxy based setters broken')
 
-    settest.set('name', 'Bernie');
+  settest.set('name', 'Bernie')
 
-    t.is(settest.get('name'), 'My name is Bernie', 'His name is not Bob, .set(key,val) setters broken');
-
-});
-
+  t.is(settest.get('name'), 'My name is Bernie', 'His name is not Bob, .set(key,val) setters broken')
+})
 
 test('observable:nested', t => {
-    t.plan(3);
+  t.plan(3)
 
-    let base = observable({
-        nest1: {
-            nest2: {}
-        }
-    });
+  let base = observable({
+    nest1: {
+      nest2: {}
+    }
+  })
 
-    t.true(base.nest1.isObservable)
+  t.true(base.nest1.isObservable)
 
-    t.true(base.nest1.nest2.isObservable)
+  t.true(base.nest1.nest2.isObservable)
 
-    base.nest1.nest2.nest3 = {};
+  base.nest1.nest2.nest3 = {}
 
-    t.true(base.nest1.nest2.nest3.isObservable)
-
-});
+  t.true(base.nest1.nest2.nest3.isObservable)
+})
 
 test('observable:on-off-once', t => {
-    //t.plan(3);
+  // t.plan(3)
 
-    t.pass();
-    let base = observable();
-
-
-
-});
+  t.pass()
+  let base = observable()
+})
